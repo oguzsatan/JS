29afe01011cfe38af4e939d03c9a88b9db4c4dff
Oguz Kaan Satan <32596175+oguzsatan@users.noreply.github.com>
2021-10-24 21:28:58 +0300

    Update HW#1--MathTable.js

--- HW#1--MathTable.js
+++ HW#1--MathTable.js
@@ -1,16 +1,16 @@
 function P(X, lambda){
     let factorial = 1;
 
-    for(let i=2; i<=X; i++){ // 1,2,3,4,5,6,7,...,x
+    for(let i=2; i<=X; i++){ // 1,2,3,4,5,6,7.x
         factorial *= i;
     }
 
     return Math.exp(-lambda) * Math.pow(lambda, X) / factorial;
 }
 
-let L = ['0.5', '1.0', '1.5', '2.0', '2.5', '3.0'];
+let L = ['0.1', '0.2', '0.3', '0.4', '0.5', '0.6','0.7','0.8','0.9','1.0'];
 let poissonTable = [];
-let x = 8;
+let x = 7;
 
 for(let i=0; i<=x; i++){ 
     poissonTable.push({});
